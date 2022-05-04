938cd705b1cd557c60219841c894726c4a4c9d46
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-04 18:51:12 -0300

    Criação do controller de Produto. Adição do método getAll

---
+++ farmacia/src/main/java/org/generation/farmacia/controller/ProdutoController.java
@@ -0,0 +1,26 @@
+package org.generation.farmacia.controller;
+
+import java.util.List;
+
+import org.generation.farmacia.model.ProdutoModel;
+import org.generation.farmacia.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin("*")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<ProdutoModel>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
