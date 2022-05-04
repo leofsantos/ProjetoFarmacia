32cc80b4d55b3042761fa1985fc18c2f82e95394
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-04 19:41:15 -0300

    Criação do método getById() no controller de Produto

--- farmacia/src/main/java/org/generation/farmacia/controller/ProdutoController.java
+++ farmacia/src/main/java/org/generation/farmacia/controller/ProdutoController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.generation.farmacia.model.ProdutoModel;
 import org.generation.farmacia.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,12 @@ public class ProdutoController {
 	@GetMapping
 	public ResponseEntity<List<ProdutoModel>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProdutoModel> getById(@PathVariable Long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
 }
